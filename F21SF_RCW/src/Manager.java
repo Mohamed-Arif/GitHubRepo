c17eb0aa85f45c68e93500526e98e50f46a098b1
Mohamed Arif <a.mdarif10@yahoo.com>
2021-07-19 16:30:09 +0530

    added readFile() code to manager.java

--- F21SF_RCW/src/Manager.java
+++ F21SF_RCW/src/Manager.java
@@ -1,9 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Manager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	SwimmerList swimmerList = new SwimmerList();	 
 
+public void readFile() {
+				
+	File inputFileObject = new File("input.txt"); 
+	Scanner scannerObject;
+				
+	try {
+		String[] lines = new String[20];
+		scannerObject = new Scanner(inputFileObject);
+				
+		for(int i=0;scannerObject.hasNextLine();i++)
+			{
+				lines[i]=scannerObject.nextLine();
+			}
+					swimmerList.readList(lines);
+					
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} 
+				
+					    
+			}		 
+		
+	
+	public static void main(String[] args)
+	{
+	
 	}
-
 }
+
