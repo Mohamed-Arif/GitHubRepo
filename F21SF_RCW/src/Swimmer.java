9c6f425e1ea9c6f61ce264cc1123bb26da86cffb
Mohamed Arif <a.mdarif10@yahoo.com>
2021-07-19 16:18:31 +0530

    Removed name class from Swimmer

--- F21SF_RCW/src/Swimmer.java
+++ F21SF_RCW/src/Swimmer.java
@@ -24,7 +24,7 @@ public Swimmer(int number, String name, String level, int age, int[] Scores)
 	this.getOverAllScoreArray();
 }
 
-// Generating getters and setters to set and return values
+// Getters and setters to set and return values
 
 public int getSwimmerNumber() {return SwimmerNumber;}
 public void setSwimmerNumber(int SwimmerNumber) {this.SwimmerNumber = SwimmerNumber;}
@@ -43,7 +43,7 @@ public void setScoreArray(int[] ScoreArray) {this.ScoreArray = ScoreArray;}
 
 public Double getOverAllScoreArray() 
 {
-	int [] Scores = this.getScoreArray(); 		// the array score value is assigned to the variable
+	int [] Scores = this.getScoreArray(); 		// the score array value is assigned to the variable
 	Arrays.sort(Scores);						// using the array method "sort", the scores are sorted in ascending order
 	double average = 0;							// initializing the value of average as 0
 	
@@ -58,7 +58,8 @@ else if(getSwimmerLevel().contentEquals("Silver"))
 }
 else if(getSwimmerLevel().contentEquals("Gold"))
 {
-	average = (Scores[Scores.length-1] + Scores[Scores.length-2] + Scores[Scores.length-3])/3.0;	// Calculating the score for Gold level
+	average = (Scores[Scores.length-1] + Scores[Scores.length-2] + 
+				Scores[Scores.length-3])/3.0;	// Calculating the score for Gold level
 }
 
 return this.OverAllScore = average;
@@ -88,14 +89,12 @@ public String getFullDetails()
 			ScoreString = ScoreString + ScoreArray[i];
 		}
 	}
-
 String fullDetails = String.format("%s is a Swimmer.\nTheir age is %d and their number is %d.\n" + 
 									"They are in the %s Level and the scores obtained are: %s\n" + 
 									"Their Final score is: %,.1f", 
 									getSwimmerName().getFullName(), getSwimmerAge(),
 									getSwimmerNumber(), getSwimmerLevel(), ScoreString, getOverAllScoreArray());
-
-return fullDetails;
+	return fullDetails;
 }
 
 public String getShortDetails()
@@ -103,85 +102,5 @@ public String getShortDetails()
 	String shortDetails = String.format("CN %d (%s) has overall score of %,.1f",
 							this.getSwimmerNumber(), this.getSwimmerName().getInitials(), this.getOverAllScoreArray());
 	return shortDetails;
+}	
 }
-
-
-
-public class Name {			// utilizing Name.java from the gitlab repo f21sf-2020-21/ programs/ 05-name2/ Name.java
-	
-	 private String firstName;
-	 private String middleName;
-	 private String lastName;
-
-	 //constructor to create object with a first and last name
-	 public Name(String fName, String lName) {
-	firstName = fName;
-	middleName = "";
-	lastName = lName;
-	 }
-
-	 //constructor to create object with first, middle and last name
-	 //if there isn't a middle name, that parameter could be an empty
-	//String
-	 public Name(String fName, String mName, String lName) {
-	firstName = fName;
-	middleName = mName;
-	lastName = lName;
-	}
-	 //constructor to create name from full name
-	 //in the format first name then space then last name
-	 //or first name then space then middle name then space then last name
-	 public Name (String fullName) {
-	 int spacePos1 = fullName.indexOf(' ');
-	 firstName = fullName.substring(0, spacePos1);
-	 int spacePos2 = fullName.lastIndexOf(' ');
-	 if (spacePos1 == spacePos2)
-	 middleName = "";
-	 else
-	 middleName = fullName.substring(spacePos1+1, spacePos2);
-	 lastName = fullName.substring(spacePos2 + 1);
-	 }
-
-	 //returns the first name
-	 public String getFirstName() {return firstName; }
-	 //returns the last name
-	 public String getLastName() {return lastName; }
-
-	 //change the last name to the value provided in the parameter
-	 public void setLastName(String ln) {
-	 lastName = ln;
-	 }
-
-	 //returns the first name then a space then the last name
-	 public String getFirstAndLastName() {
-	return firstName + " " + lastName;
-	 }
-
-	 //returns the last name followed by a comma and a space
-	 // then the first name
-	 public String getLastCommaFirst() {
-	return lastName + ", "+ firstName;
-	 }
-
-	 //returns the full name
-	
-	 //either first name then space then last name
-	 //or first name then space then middle name then space
-	 // and then last name
-	 public String getFullName() {
-	 String result = firstName + " ";
-	 if (!middleName.equals("")) {
-	 result += middleName + " ";
-	 }
-	 result += lastName;
-	 return result;
-	}
-	public String getInitials() {
-		if(middleName =="") {
-			return String.format("%s%s",firstName.charAt(0) , lastName.charAt(0));
-			
-		}
-		else {
-		return String.format("%s%s%s",firstName.charAt(0) , middleName.charAt(0) , lastName.charAt(0));
-	}
-}}}
