5a4a98bf054e74db36d760942db529805f45cd55
Mohamed Arif <a.mdarif10@yahoo.com>
2021-07-19 16:30:29 +0530

    wrote some code for SwimmerList

--- F21SF_RCW/src/SwimmerList.java
+++ F21SF_RCW/src/SwimmerList.java
@@ -1,4 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SwimmerList {
+	
+private Swimmer[] SwimmerArray = new Swimmer[15];
+int size = 0;
 
+public void readList(String[] lines)
+{
+	for (int i = 0; lines[i] != null;i++)
+	{ 
+		String words [] = lines[i].split(", ");
+		int intScores [] = new int[5];
+		String strScores [] = words[5].split(",");
+		
+		for(int j = 0; j< strScores.length; j++)
+		{
+			intScores[j]= Integer.parseInt(strScores[j]);
+		}
+	}
+	this.SwimmerArray[i] = new Swimmer(Integer.parseInt(words[0]),words[1],
+			Integer.parseInt(words[2]), words[3], words[4], intScores);
+}
 }
