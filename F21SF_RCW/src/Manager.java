8e5ea590a83bedf672396f13b676f80f2f3b095c
Mohamed Arif <a.mdarif10@yahoo.com>
2021-07-26 21:42:03 +0530

    completed manager

--- F21SF_RCW/src/Manager.java
+++ F21SF_RCW/src/Manager.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class Manager {
@@ -9,9 +8,25 @@ public class Manager {
 		ReaderWriter rw = new ReaderWriter();
 		rw.readFile("input.txt", swimmah);
 		
+		String report = swimmah.getReport();
+		
+		rw.writeFile("output.txt", report);
+		
+		for( int num = 0; num < 3; num++)
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the swimmer number to retrieve their details");
+			String id = sc.next();
+			sc.close();
+			if(swimmah.checkSwim(id) != null)
+			{
+				System.out.println(swimmah.checkSwim(id));
+			}
+			else
+			{
+				System.out.println("Please check the number you have entered");
+			}
+		}
 	
-	}
-	
-	
+	}	
 }
-
